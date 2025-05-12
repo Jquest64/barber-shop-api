20a1ae63c71296edb94b80d4152326652b99f0ad
Marcio Farias Pinto <mjquest64@outlook.com>
2025-05-12 16:40:39 -0300

    Testes finais
    Testes finais na API e no front-end.

--- src/main/java/br/com/dio/barbershopapi/controller/ScheduleController.java
+++ src/main/java/br/com/dio/barbershopapi/controller/ScheduleController.java
@@ -2,6 +2,7 @@ package br.com.dio.barbershopapi.controller;
 
 import br.com.dio.barbershopapi.controller.request.SaveScheduleRequest;
 import br.com.dio.barbershopapi.controller.response.SaveScheduleResponse;
+import br.com.dio.barbershopapi.controller.response.ScheduleAppointmentMonthResponse;
 import br.com.dio.barbershopapi.mapper.IScheduleMapper;
 import br.com.dio.barbershopapi.service.IScheduleService;
 import br.com.dio.barbershopapi.service.query.IScheduleQueryService;
@@ -34,6 +35,21 @@ public class ScheduleController {
     @ResponseStatus(NO_CONTENT)
     void delete(@PathVariable final long id){
         service.delete(id);
+    }
+
+    @GetMapping("{year}/{month}")
+    ScheduleAppointmentMonthResponse listMonth(@PathVariable final int year, @PathVariable final int month){
+
+        var yearMonth = yearMonth.of(year, month);
+        var startAt = yearMonth.atDay(1)
+                .atTime(0, 0, 0, 0)
+                .atOffset(UTC);
+        var endAt = yearMonth.atEndOfMonth()
+                .atTime(23, 59, 59, 999_999_999)
+                .atOffset(UTC);
+        var entities = queryService.findInMonth(startAt, endAt);
+
+        return mapper.toMonthResponse(year, month, entities);
     }
 
 }
